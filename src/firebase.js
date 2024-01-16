b4b20932c2753233464d29c1122bfd47c2d3c7b3
HmUmaxKhan <mukhan.bese22seecs@seecs.edu.pk>
2024-01-16 17:03:22 +0500

    2nd commit

---
+++ src/firebase.js
@@ -0,0 +1,65 @@
+import { initializeApp, getApps, getApp } from 'firebase/app'
+import {
+    getMessaging,
+    getToken,
+    onMessage,
+    isSupported,
+} from 'firebase/messaging'
+import { useStoreFcm } from './hooks/react-query/push-notification/usePushNotification'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyASnnV4YxKYKTXJkYysPsFpDkVPaWGtfMw",
+    authDomain: "droproot-388511.firebaseapp.com",
+    projectId: "droproot-388511",
+    storageBucket: "droproot-388511.appspot.com",
+    messagingSenderId: "464590656003",
+    appId: "1:464590656003:web:f196857c82787200c37436",
+    measurementId: "G-7MJHLQXNWT"
+}
+const firebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+const messaging = (async () => {
+    try {
+        const isSupportedBrowser = await isSupported()
+        if (isSupportedBrowser) {
+            return getMessaging(firebaseApp)
+        }
+
+        return null
+    } catch (err) {
+        return null
+    }
+})()
+
+export const fetchToken = async (setTokenFound, setFcmToken) => {
+    return getToken(await messaging, {
+        vapidKey:
+            '',
+    })
+        .then((currentToken) => {
+            if (currentToken) {
+                setTokenFound(true)
+                setFcmToken(currentToken)
+
+                // Track the token -> client mapping, by sending to backend server
+                // show on the UI that permission is secured
+            } else {
+                setTokenFound(false)
+                setFcmToken()
+                // shows on the UI that permission is required
+            }
+        })
+        .catch((err) => {
+            console.error(err)
+            // catch error while creating client token
+        })
+}
+
+export const onMessageListener = async () =>
+    new Promise((resolve) =>
+        (async () => {
+            const messagingResolve = await messaging
+            onMessage(messagingResolve, (payload) => {
+                resolve(payload)
+            })
+        })()
+    )
